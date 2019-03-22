22ab5214891a43349eeee4b0b513e064423953e0
lilun <lilun@xiaomi.com>
2019-03-22 16:03:07 +0800

    Initial Commit

---
+++ TestShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestShare
+//
+//  Created by lilun on 2019/3/22.
+//  Copyright © 2019年 lilun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
